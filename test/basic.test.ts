8c3c610afb643521460e684a090d7813f4e20bc7
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2023-02-13 10:53:31 +0100

    refactor: lint

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
-import { setup, $fetch } from '@nuxt/test-utils'
+import { describe, expect, it } from 'vitest'
+import { $fetch, setup } from '@nuxt/test-utils'
 
 describe('ssr', async () => {
   await setup({
-    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url))
   })
 
   it('renders the index page', async () => {
