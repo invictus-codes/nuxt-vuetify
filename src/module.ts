e23361ef3803b2e206562e0d9259631792d2d68b
Jens van Hellemondt <jens@invictus.codes>
2023-03-22 09:22:31 +0100

    lint: update indenting

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,10 @@
 import {
-    addPluginTemplate,
-    createResolver,
-    defineNuxtModule,
-    getNuxtVersion,
-    isNuxt3,
-    useLogger,
+  addPluginTemplate,
+  createResolver,
+  defineNuxtModule,
+  getNuxtVersion,
+  isNuxt3,
+  useLogger,
 } from '@nuxt/kit'
 import defu from 'defu'
 import vuetify from 'vite-plugin-vuetify'
@@ -17,116 +17,116 @@ const logger = useLogger('nuxt:vuetify')
 export type TVuetifyOptions = Partial<VuetifyOptions> & { ssr: boolean; treeshaking: boolean }
 
 export interface ModuleOptions {
-    moduleOptions: {
-        useIconCDN?: boolean
-        treeshaking?: boolean
-        autoImport?: boolean
-        styles?: true | 'none' | 'expose' | 'sass' | {
-            configFile: string
-        }
+  moduleOptions: {
+    useIconCDN?: boolean
+    treeshaking?: boolean
+    autoImport?: boolean
+    styles?: true | 'none' | 'expose' | 'sass' | {
+      configFile: string
     }
-    vuetifyOptions?: VuetifyOptions
+  }
+  vuetifyOptions?: VuetifyOptions
 }
 
 export default defineNuxtModule<ModuleOptions>({
-    // Default configuration options of the Nuxt module
-    defaults: {
-        moduleOptions: {
-            // Nuxt-Vuetify module
-            useIconCDN: true,
-            treeshaking: false,
-            // Vite-plugin-vuetify
-            styles: true,
-            autoImport: true,
-        },
-        vuetifyOptions: undefined,
+  // Default configuration options of the Nuxt module
+  defaults: {
+    moduleOptions: {
+      // Nuxt-Vuetify module
+      useIconCDN: true,
+      treeshaking: false,
+      // Vite-plugin-vuetify
+      styles: true,
+      autoImport: true,
     },
-    meta: {
-        name,
-        version,
-        configKey: CONFIG_KEY,
-        compatibility: {
-            nuxt: '^3.0.0',
-        },
+    vuetifyOptions: undefined,
+  },
+  meta: {
+    name,
+    version,
+    configKey: CONFIG_KEY,
+    compatibility: {
+      nuxt: '^3.0.0',
     },
-    setup({
-        moduleOptions,
+  },
+  setup({
+    moduleOptions,
         vuetifyOptions: _vuetifyOptions,
-    }, nuxt) {
-        if (!isNuxt3(nuxt)) {
-            logger.error(`Cannot support nuxt version: ${getNuxtVersion(nuxt)}`)
-        }
+  }, nuxt) {
+    if (!isNuxt3(nuxt)) {
+      logger.error(`Cannot support nuxt version: ${getNuxtVersion(nuxt)}`)
+    }
 
-        const {
+    const {
+      styles,
+      autoImport,
+      useIconCDN,
+      treeshaking,
+    } = moduleOptions
+
+    // Prepare options for the runtime plugin
+    const isSSR = nuxt.options.ssr
+    const vuetifyOptions = <TVuetifyOptions>defu(_vuetifyOptions, {
+      ssr: isSSR,
+      treeshaking,
+    })
+
+    // Module: Transpile the runtime and vuetify package
+    const resolver = createResolver(import.meta.url)
+    const runtimeDir = resolver.resolve('./runtime')
+    nuxt.options.build.transpile.push(runtimeDir)
+    nuxt.options.build.transpile.push(CONFIG_KEY)
+
+    // Vuetify: add vuetify styles to the nuxtApp css
+    // @TODO: this is not SSR friendly (styles are added twice.
+    //        also can't do this in plugin with dynamic import (all styles are included).
+    nuxt.options.css ??= []
+    if (typeof styles === 'string' && ['sass', 'expose'].includes(styles)) {
+      nuxt.options.css.unshift('vuetify/styles/main.sass')
+    } else if (styles === true) {
+      nuxt.options.css.unshift('vuetify/styles')
+    } else if (isSSR && !treeshaking && typeof styles === 'object' && styles?.configFile && typeof styles.configFile === 'string') {
+      nuxt.options.css.unshift(styles.configFile)
+    }
+
+    // Icon CDN's
+    if (useIconCDN) {
+      const iconCDNs = new Map()
+      iconCDNs.set('fa', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@latest/css/all.min.css')
+      iconCDNs.set('mdi', 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css')
+      iconCDNs.set('md', 'https://fonts.googleapis.com/css?family=Material+Icons')
+      nuxt.options.app.head.link ||= []
+      nuxt.options.app.head.link.push({
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: iconCDNs.get(vuetifyOptions.icons?.defaultSet || 'mdi'),
+      })
+    }
+
+    // Module: use vite-plugin-vuetify for treeshaking and includes autoImport and styles
+    nuxt.hook('vite:extendConfig', (config) => {
+      config.optimizeDeps = defu(config.optimizeDeps, { exclude: ['vuetify'] })
+
+      if (treeshaking) {
+        config.plugins = [
+          ...(Array.isArray(config.plugins) ? config.plugins : []),
+          vuetify({
             styles,
             autoImport,
-            useIconCDN,
-            treeshaking,
-        } = moduleOptions
+          }),
+        ]
+      }
 
-        // Prepare options for the runtime plugin
-        const isSSR = nuxt.options.ssr
-        const vuetifyOptions = <TVuetifyOptions>defu(_vuetifyOptions, {
-            ssr: isSSR,
-            treeshaking,
-        })
+      config.ssr ||= {}
+      config.ssr.noExternal = [
+        ...(Array.isArray(config.ssr.noExternal) ? config.ssr.noExternal : []),
+        CONFIG_KEY,
+      ]
+    })
 
-        // Module: Transpile the runtime and vuetify package
-        const resolver = createResolver(import.meta.url)
-        const runtimeDir = resolver.resolve('./runtime')
-        nuxt.options.build.transpile.push(runtimeDir)
-        nuxt.options.build.transpile.push(CONFIG_KEY)
-
-        // Vuetify: add vuetify styles to the nuxtApp css
-        // @TODO: this is not SSR friendly (styles are added twice.
-        //        also can't do this in plugin with dynamic import (all styles are included).
-        nuxt.options.css ??= []
-        if (typeof styles === 'string' && ['sass', 'expose'].includes(styles)) {
-            nuxt.options.css.unshift('vuetify/styles/main.sass')
-        } else if (styles === true) {
-            nuxt.options.css.unshift('vuetify/styles')
-        } else if (isSSR && !treeshaking && typeof styles === 'object' && styles?.configFile && typeof styles.configFile === 'string') {
-            nuxt.options.css.unshift(styles.configFile)
-        }
-
-        // Icon CDN's
-        if (useIconCDN) {
-            const iconCDNs = new Map()
-            iconCDNs.set('fa', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@latest/css/all.min.css')
-            iconCDNs.set('mdi', 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css')
-            iconCDNs.set('md', 'https://fonts.googleapis.com/css?family=Material+Icons')
-            nuxt.options.app.head.link ||= []
-            nuxt.options.app.head.link.push({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: iconCDNs.get(vuetifyOptions.icons?.defaultSet || 'mdi'),
-            })
-        }
-
-        // Module: use vite-plugin-vuetify for treeshaking and includes autoImport and styles
-        nuxt.hook('vite:extendConfig', (config) => {
-            config.optimizeDeps = defu(config.optimizeDeps, { exclude: ['vuetify'] })
-
-            if (treeshaking) {
-                config.plugins = [
-                    ...(Array.isArray(config.plugins) ? config.plugins : []),
-                    vuetify({
-                        styles,
-                        autoImport,
-                    }),
-                ]
-            }
-
-            config.ssr ||= {}
-            config.ssr.noExternal = [
-                ...(Array.isArray(config.ssr.noExternal) ? config.ssr.noExternal : []),
-                CONFIG_KEY,
-            ]
-        })
-
-        addPluginTemplate({
-            src: resolver.resolve(runtimeDir, 'templates/plugin.mts'),
-            options: vuetifyOptions,
-        })
-    },
+    addPluginTemplate({
+      src: resolver.resolve(runtimeDir, 'templates/plugin.mts'),
+      options: vuetifyOptions,
+    })
+  },
 })
